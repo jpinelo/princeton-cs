a1a9703004e2e6730a6ef1fff45867a01c85c61a
jpinelo <j.pinelo@mac.com>
2021-04-03 13:57:52 +0000

    end

--- DiscreteDistribution.java
+++ DiscreteDistribution.java
@@ -12,24 +12,48 @@ public class DiscreteDistribution {
             n[i] = Integer.parseInt(args[i + 1]);
         }
 
-        int[] p = new int[n.length];
+        //        int m = Integer.parseInt(args[0]);
+        //        int n = args.length;
+        //        int[] cumSum = new int[n];
+
+        // cumsums along array
+        int[] s = new int[n.length];
         int sum = 0;
-        for (int i = 0; i < p.length; i++) {
-            sum = sum + n[i];
-            p[i] = sum;
-
+        for (int i = 0; i < s.length; i++) {
+            sum += n[i];
+            s[i] = sum;
         }
 
-        for (int i = 0; i < n.length; i++) {
-            System.out.print(p[i] + " ");
-            //  System.out.print(n.length);
+        // read arguments from args_1 to args_n-1 and update cumulativeSum
+        //        for (int i = 1; i < n; i++) {
+        //            // update cumulativeSum
+        //            cumSum[i] = cumSum[i - 1] + Integer.parseInt(args[i]);
+        //        }
+
+        for (int i = 0; i < m; i++) {
+            // pick a random integer r uniformly between 0 and S_n
+            int r = (int) (Math.random() * s[n.length - 1]);
+            // initialize index k to be the middle index between 0 to n.
+            int max = n.length - 1;
+            int min = 0;
+            int k = (n.length - 1) / 2;
+
+            while (k != 0 && (r < s[k - 1] || r >= s[k])) {
+                if (s[k - 1] > r) {
+                    max = k;
+                    k = (max + min) / 2;
+                }
+                else {
+                    min = k;
+                    k = (max + min) / 2;
+                    if (min == k) {
+                        k++;
+                    }
+                }
+            }
+            // output
+            System.out.print(k + " ");
         }
-
-        //        int max = n.length - 1;
-        //        int min = 1;
-
-        //            for (int i = 0; i < m; i++) {
-        //                System.out.print(Math.round(Math.random() * (max - min + 1) + min) + " ");
-        //            }
     }
+
 }
