1f655d74ea93becb4c19672ac125c4ee6242b832
jpinelo <j.pinelo@mac.com>
2021-04-03 16:20:23 +0000

    functioning

--- ThueMorse.java
+++ ThueMorse.java
@@ -7,8 +7,24 @@
 public class ThueMorse {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int[] tm = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
+        //        int[] tm = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
+        // create tm seq of length n
+        // create string seq
+        String thue = "0";
+        String morse = "1";
+        for (int i = 0; i <= n; i++) {
+            String t = thue;
+            String m = morse;
+            thue += m;
+            morse += t;
+        }
+        // from string to char array
+        char[] tm = new char[thue.length()];
+        for (int i = 0; i < thue.length(); i++) {
+            tm[i] = thue.charAt(i);
+        }
 
+        // create matrix pattern
         for (int i = 0; i < n; i++) { // height
             for (int j = 0; j < n; j++) { // width
                 if (tm[i] == tm[j]) {
