7ad3edd5579fe1746bb87cd32f566d0fbb99043b
jpinelo <j.pinelo@mac.com>
2021-04-03 12:09:59 +0000

    cumsum

--- DiscreteDistribution.java
+++ DiscreteDistribution.java
@@ -7,8 +7,29 @@
 public class DiscreteDistribution {
     public static void main(String[] args) {
         int m = Integer.parseInt(args[0]);
-        int[] n = Integer.parseInt(args)
+        int[] n = new int[args.length - 1];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = Integer.parseInt(args[i + 1]);
+        }
 
+        int[] p = new int[n.length];
+        int sum = 0;
+        for (int i = 0; i < p.length; i++) {
+            sum = sum + n[i];
+            p[i] = sum;
 
+        }
+
+        for (int i = 0; i < n.length; i++) {
+            System.out.print(p[i] + " ");
+            //  System.out.print(n.length);
+        }
+
+        //        int max = n.length - 1;
+        //        int min = 1;
+
+        //            for (int i = 0; i < m; i++) {
+        //                System.out.print(Math.round(Math.random() * (max - min + 1) + min) + " ");
+        //            }
     }
 }
