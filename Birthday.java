1102f29d96871238cad7f5cd4aa080faee9a987a
jpinelo <j.pinelo@mac.com>
2021-04-04 10:55:04 +0000

    implement_random

--- Birthday.java
+++ Birthday.java
@@ -6,6 +6,16 @@
 
 public class Birthday {
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        //   int[] room =
+        
+        // create random in range
+        int max = n - 1;
+        int min = 1;
+        int range = max - min + 1;
+        int r = (int) (Math.random() * range + min);
 
+        System.out.println(r);
     }
 }
